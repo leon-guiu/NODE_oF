f4bb3fbf6345a7c9d44ff4a9663bf202b41e8378
Fernando León-Guiu <leon-guiu@hotmail.com>
2015-09-16 16:40:49 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Fernando Leon-Guiu on 16/09/2015.
+//  Copyright (c) 2015 Fernando Leon-Guiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
